daaf2b894ea84626b5cd62de194b0f8a9e017e57
Jonas Sebastian <jonas.sebastian@dlsau.edu.ph>
2024-08-01 03:42:32 +0800

    implemented screen size scaling for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,22 @@ export default function Navbar() {
           sx={{
             fontWeight: 'bold',
             fontFamily: 'RetroComputer',
-            fontSize: '28px',
-            border: '4px solid white',
-            borderRadius: '15px',
-            padding: '10px 20px',
+            fontSize: {
+              xs: '1.5vh',    // Extra small screens or Phone Screens
+              sm: '1.5vh',  // Small screens or Small Tablet Screens or Foldable Phone Screens (Samsung Fold)
+              md: '2vh',    // Medium screens or Large Tablets (Ipad Pro size) 
+              lg: '2.5vh',  // Large screens or Low Resolution Monitors
+              xl: '1.75vh'     // Extra large screens or High Resolution Monitors
+            },
+            border: '3px solid white',
+            borderRadius: '10px',
+            padding: {
+              xs: '0px 5px',
+              sm: '5px 10px',
+              md: '8px 15px',
+              lg: '10px 20px',
+              xl: '10px 20px'
+            },
           }}
         >
           Blue Archive
@@ -33,8 +45,20 @@ export default function Navbar() {
           sx={{
             fontWeight: 'bold',
             fontFamily: 'RetroComputer',
-            fontSize: '20px',
-            margin: '0 24px',
+            fontSize: {
+              xs: '1.25vh',
+              sm: '1.5vh',
+              md: '2vh',
+              lg: '2.5vh',
+              xl: '1.75vh' 
+            },
+            margin: {
+              xs: '0 4px',   // Extra small screens
+              sm: '0 6px',   // Small screens
+              md: '0 12px',   // Medium screens
+              lg: '0 24px',  // Large screens
+              xl: '0 24px'   // Extra large screens
+            },
           }}
         >
           Total Assault
@@ -47,8 +71,20 @@ export default function Navbar() {
           sx={{
             fontWeight: 'bold',
             fontFamily: 'RetroComputer',
-            fontSize: '20px',
-            margin: '0 24px',
+            fontSize: {
+              xs: '1.25vh',
+              sm: '1.5vh',
+              md: '2vh',
+              lg: '2.5vh',
+              xl: '1.75vh' 
+            },
+            margin: {
+              xs: '0 4px',   // Extra small screens
+              sm: '0 6px',   // Small screens
+              md: '0 12px',   // Medium screens
+              lg: '0 24px',  // Large screens
+              xl: '0 24px'   // Extra large screens
+            },
           }}
         >
           Grand Assault
@@ -56,4 +92,4 @@ export default function Navbar() {
       </Toolbar>
     </AppBar>
   );
-};
+}
